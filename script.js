7efbcb6fa32acc1a453bb0fee85b19fc2c93084f
Elie Ariza Sosa <sosa.elie12@gmail.com>
2025-03-23 11:47:15 -0400

    Finished the progress bar for the most part.
    Still need to add background image and more design elements.
    Progress bar kinda glitches a bit when the user scrolls really fast up or down.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function main(){
+    const header = document.getElementById("header");
+    const progressBar = document.getElementById("progress-bar");
+    const progressBarContainer = document.getElementById("progress-container");
+    const contents = document.querySelectorAll(".content");
+    const stepCircles = document.querySelectorAll(".step-circle");
+    const progressBarContainerWidth = progressBarContainer.clientWidth;
+    //Determine the distance between each step circle, from left border to left border
+    const distancesAsPercentages = [];
+    const distances = [];
+    for(let i = 0; i < stepCircles.length-1; i++){
+        const currentCircleLeftBorder = stepCircles[i].getBoundingClientRect().left;
+        const nextCircleLeftBorder = stepCircles[i+1].getBoundingClientRect().left;
+        const distance = nextCircleLeftBorder - currentCircleLeftBorder;
+        const content = contents[i];
+        const currentPercent = distance/progressBarContainerWidth
+        const lastPercent = i === 0?0:distancesAsPercentages[i-1];
+        const totalPercent = currentPercent + lastPercent;
+        distancesAsPercentages.push(totalPercent);
+        distances.push(distance);
+        content.percentOfProgressbar = currentPercent;
+        stepCircles[i].percentOfProgressbar = currentPercent;
+        content.totalPercentOfProgressbar = totalPercent;
+        stepCircles[i].totalPercentOfProgressbar = totalPercent;
+    }
     window.addEventListener("scroll", e => {
         const currentY = window.scrollY;
         
@@ -6,11 +30,45 @@ function main(){
         // of the body element is if it were taking up the entire screen height
         // The client height is the actual visible portion of the document body
         // The totalY is therefore the remainder that needs to be scrolled into/out of 
-        const totalY = document.body.scrollHeight - document.body.clientHeight;
+        // const totalY = document.body.scrollHeight - document.body.clientHeight;
 
-        const percentScrolled = (currentY/totalY)*100;
-        const progressBar = document.getElementById("progress-bar");
-        progressBar.style.width = `${percentScrolled}%`;
+        // const percentScrolled = (currentY/totalY)*100;
+        const headerHeight = header.clientHeight;
+        
+        contents.forEach((content, i) =>  {
+            const topBorderPosition = content.getBoundingClientRect().y;
+            const contentHeight = content.clientHeight;
+            const bottomBorderPosition = topBorderPosition + contentHeight;
+            
+            //If the content hasn't reached the bottom border of the header then don't calculate anything for it yet
+            //Or if the content bottom is past the bottom border of the header then ignore it
+            if (topBorderPosition > headerHeight){
+                if(stepCircles[i].classList.contains("filled"))stepCircles[i].classList.remove("filled");
+                return;
+            }
+            else if(topBorderPosition <= headerHeight){
+                if(!stepCircles[i].classList.contains("filled"))stepCircles[i].classList.add("filled");
+            }
+            if(bottomBorderPosition <= headerHeight) return;
+            
+            //Determine the percent of the content that has been scrolled past
+            const contentPercentScrolled = 100 - (((bottomBorderPosition-headerHeight)/contentHeight) * 100);
+            const portionOfPercentBarForThisContent = contentPercentScrolled * content.percentOfProgressbar;
+            
+            if(i > 0){
+                const previousTotalContentPercentage = contents[i-1].totalPercentOfProgressbar;
+                const progressBarFill = portionOfPercentBarForThisContent + (previousTotalContentPercentage*100);
+                progressBar.style.width = `${progressBarFill}%`;
+                progressBar.currentPercent = progressBarFill;
+            }
+            else{
+                const progressBarFill = portionOfPercentBarForThisContent;
+                progressBar.style.width = `${progressBarFill}%`;
+                progressBar.currentPercent = progressBarFill;
+            }
+            
+        })
+
     })
 }
 
