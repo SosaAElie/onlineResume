6104e90eb424483c4d062f2645cbfc4cf692a445
SosaAElie <sosa.elie12@gmail.com>
2025-03-23 21:48:00 -0400

    Fixing CSS for all iPad, iPhone and monitors.

--- script.js
+++ script.js
@@ -24,15 +24,6 @@ function main(){
         stepCircles[i].totalPercentOfProgressbar = totalPercent;
     }
     window.addEventListener("scroll", e => {
-        const currentY = window.scrollY;
-        
-        // The scroll height of the document is what the entire height 
-        // of the body element is if it were taking up the entire screen height
-        // The client height is the actual visible portion of the document body
-        // The totalY is therefore the remainder that needs to be scrolled into/out of 
-        // const totalY = document.body.scrollHeight - document.body.clientHeight;
-
-        // const percentScrolled = (currentY/totalY)*100;
         const headerHeight = header.clientHeight;
         
         contents.forEach((content, i) =>  {
@@ -43,13 +34,17 @@ function main(){
             //If the content hasn't reached the bottom border of the header then don't calculate anything for it yet
             //Or if the content bottom is past the bottom border of the header then ignore it
             if (topBorderPosition > headerHeight){
-                if(stepCircles[i].classList.contains("filled"))stepCircles[i].classList.remove("filled");
+                requestAnimationFrame(()=>{
+                    if(stepCircles[i].classList.contains("filled"))stepCircles[i].classList.remove("filled");
+                })
                 return;
             }
             else if(topBorderPosition <= headerHeight){
-                if(!stepCircles[i].classList.contains("filled"))stepCircles[i].classList.add("filled");
+                requestAnimationFrame(()=>{
+                    if(!stepCircles[i].classList.contains("filled"))stepCircles[i].classList.add("filled");
+                })                    
             }
-            if(bottomBorderPosition <= headerHeight) return;
+            if(bottomBorderPosition < headerHeight) return;
             
             //Determine the percent of the content that has been scrolled past
             const contentPercentScrolled = 100 - (((bottomBorderPosition-headerHeight)/contentHeight) * 100);
@@ -58,12 +53,16 @@ function main(){
             if(i > 0){
                 const previousTotalContentPercentage = contents[i-1].totalPercentOfProgressbar;
                 const progressBarFill = portionOfPercentBarForThisContent + (previousTotalContentPercentage*100);
-                progressBar.style.width = `${progressBarFill}%`;
+                requestAnimationFrame(()=>{
+                    progressBar.style.width = `${progressBarFill}%`;
+                })
                 progressBar.currentPercent = progressBarFill;
             }
             else{
                 const progressBarFill = portionOfPercentBarForThisContent;
-                progressBar.style.width = `${progressBarFill}%`;
+                requestAnimationFrame(()=>{
+                    progressBar.style.width = `${progressBarFill}%`;
+                })
                 progressBar.currentPercent = progressBarFill;
             }
             
